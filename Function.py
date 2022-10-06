1dbe3dfb4ca956f81cd2b20b0debcf82e0fffece
Anton Morozov <mail@aviatorstudio.ru>
2022-10-06 12:23:07 +0300

    Init

---
+++ Function.py
@@ -0,0 +1,21 @@
+a = input("Salary: ")
+
+def procent():
+    x = (int(a) * 0.1494)     # Налог от нетто
+    y = (int(a) + x)          # Сумма нетто
+    z = (int(a) * 0.40)       # Аванс
+    print("Налог от суммы нетто:", (f'{round(x)}'), "рублей.")
+    print("Сумма нетто:", (f'{round(y)}'), "рублей.")
+    print("Аванс:", (f'{round(z)}'), "рублей.")
+
+procent()
+
+print()
+print("----------")
+print()
+
+def avans(w):
+    money = w * 100 / 40
+    print("Сумма:", (f'{round(money)}'), "рублей.")
+
+avans(18640)
