67618b89722f25b2921aedf689831ba60976b499
jazzofenik <79652846424@yandex.ru>
2022-10-08 17:48:33 +0300

    islower!

--- Zadanie-5.py
+++ Zadanie-5.py
@@ -1,6 +1,6 @@
-text = input("Введите текст с разной высотой букв: ")
-
-print(f'{sum(map(str.isupper, text))}')
+# text = input("Введите текст с разной высотой букв: ")
+#
+# print(f'{sum(map(str.isupper, text))}')
 print("")
 print("------------------------------------------------")
 print("")
@@ -8,7 +8,7 @@ text = input("Введите текст с разной высотой букв:
 upper = 0
 i = 0
 while i < len(text):
-    if text[i].isupper():
+    if text[i].islower():
         upper += 1
     i += 1
 print(upper)
