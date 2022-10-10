6aaed95c3bc5922a8b78f4f525e810116723f818
Anton Morozov <mail@aviatorstudio.ru>
2022-10-10 22:02:50 +0300

    Открыл закомментированный код

--- Uprazhnenie.py
+++ Uprazhnenie.py
@@ -1,22 +1,22 @@
-# while True:
-#     a = int(input())
-#     if a < 50:
-#         continue
-#     if a > 100:
-#         break
-#     print(a)
-# print("Работа программы завершена")
+while True:
+    a = int(input())
+    if a < 50:
+        continue
+    if a > 100:
+        break
+    print(a)
+print("Работа программы завершена")
 
-# print("-----------")
-#
-# a = 1
-# sum = 0
-# while a !=0:
-#     a = int(input())
-#     sum += a
-# print(sum)
-#
-# print("-----------")
+print("-----------")
+
+a = 1
+sum = 0
+while a != 0:
+    a = int(input())
+    sum += a
+print(sum)
+
+print("-----------")
 
 a = int(input())
 b = int(input())
@@ -34,11 +34,11 @@ for i in range(a, b + 1):
             print()
 
 print("-----------")
-# sum = int(input())
-# n = int(input())
-# for i in range(1, n + 1):
-#     sum += sum * 0.07
-# print(round(sum, 2))
+sum = int(input())
+n = int(input())
+for i in range(1, n + 1):
+    sum += sum * 0.07
+print(round(sum, 2))
 
 numbers = list(range(1, 11))
 
