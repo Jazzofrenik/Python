828070886523e671dfd3064b5f19789089322794
Anton Morozov <mail@aviatorstudio.ru>
2022-10-13 17:43:29 +0300

    Рабочий прототип

--- seasons.py
+++ seasons.py
@@ -1,11 +1,20 @@
-seasons = ['autumn', 'winter', 'spring', 'summer']
-x = int(input('Type number of month: '))
+autumn = {'autumn': {'september': 9, 'october': 10, 'november': 11}}
+winter = {'winter': {'december': 12, 'january': 1, 'february': 2}}
+spring = {'spring': {'march': 3, 'april': 4, 'may': 5}}
+summer = {'summer': {'june': 6, 'july': 7, 'august': 8}}
 
-if x <= 2 or x == 12:
-    print(seasons[1])
-if 3 <= x <= 5:
-    print(seasons[2])
-if 6 <= x <= 8:
-    print(seasons[3])
-if 9 <= x <= 11:
-    print(seasons[0])
+# x = int(input('Type number of month: '))
+#
+#
+#
+# if x == 6 or x <= 8:
+#     print(summer['summer']['june'])
+# # elif 3 <= x <= 5:
+# #     print(seasons[2])
+# # elif 6 <= x <= 8:
+# #     print(seasons[3])
+# # elif 9 <= x <= 11:
+# #     print(seasons[0])
+# else:
+#     print('Нет такого времени года.')
+print(autumn, '\n', summer, '\n', spring, '\n', winter)
